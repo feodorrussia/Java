c821687e751b4313188c4f6489798da54f23d6e8
feodorrussia <fvdelows@mail.ru>
2023-09-04 09:55:35 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+//        Иванов Денис Юрьевич
+//        denis.ivanov@mail.ru
+    }
+}
